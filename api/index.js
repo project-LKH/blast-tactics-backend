91a3e479cf05c1fac447e10993c9d4795a017154
Liam Henry <Liam.Henry@umuzi.org>
2025-03-03 14:12:30 +0000

    set up for vercel hosting

--- api/index.js
+++ api/index.js
@@ -2,7 +2,6 @@ const express = require("express")
 const cors = require("cors")
 
 const app = express()
-const PORT = process.env.PORT || 5000
 app.use(cors())
 app.use(express.json())
 
@@ -35,6 +34,5 @@ app.post("/move", (req, res) => {
     res.json({ gameState: game })
 })
 
-app.listen(PORT, "0.0.0.0", () => {
-    console.log(`✅ Server running on http://localhost:${PORT}`)
-})
+
+module.exports = app
